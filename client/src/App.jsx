88909e909ac2f4fc1d198694f2addbae227e53a0
Diwakar <diwakar@gmail.com>
2024-01-15 20:38:36 +0530

    add upload form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,36 @@
+import { useState } from "react";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import Upload from "./components/Upload";
 
-const photos =[
-'https://picsum.photos/id/237/536/354' ,
-'https://picsum.photos/seed/picsum/536/354',
-'https://picsum.photos/id/1084/536/354?grayscale',
-'https://picsum.photos/id/1060/536/354?blur=2',
-'https://picsum.photos/458/354?image=0',
-'https://picsum.photos/536/354'
-]
+
+const photos = [
+  "https://picsum.photos/id/237/536/354",
+  "https://picsum.photos/seed/picsum/536/354",
+  "https://picsum.photos/id/1084/536/354?grayscale",
+  "https://picsum.photos/id/1060/536/354?blur=2",
+  "https://picsum.photos/458/354?image=0",
+  "https://picsum.photos/536/354",
+];
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [collapse, isCollapse] = useState(false)
+
+  const toggle = () => isCollapse(!collapse)
+
   return (
     <>
-     <Navbar/>
+      <Navbar />
       <div className="container text-center mt-5">
+        <button className="btn btn-success float-end" onClick={toggle}>{collapse ? 'Close':'+ Add'}</button>
+        <div className="clearfix mb-4"></div>
+        <Upload isVisible={collapse}/>
         <h1>Gallery</h1>
         <div className="row">
-          {photos.map((photo, i) =><Card key={i} src={photo}/>
-         )}
-        
+          {items.map((photo, i) => (
+            <Card key={i} src={photo} />
+          ))}
         </div>
       </div>
     </>
