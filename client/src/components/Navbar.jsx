2b0da6dadbb5e4c9c0143b3e4e4ea1de0835fee0
Diwakar <diwakar@gmail.com>
2024-01-15 20:07:14 +0530

    breakdowns in components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,45 +1,37 @@
 import React from 'react'
 
-const Navbar = () => {
-  return (
-    <div>
-       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        ⚡ Firestock
+function Navigation() {
+  return(
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    {/* remove all links except HOME */}
+    <li className="nav-item">
+      <a className="nav-link active" aria-current="page" href="#">
+        Home
       </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          {/* remove all links except HOME */}
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-          </li>
-        </ul>
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
-        <ul className="navbar-nav mb-2 mb-lg-0">
+    </li>
+  </ul>
+  )
+}
+
+function SearchForm() {
+  return(
+    <form className="d-flex">
+    <input
+      className="form-control me-2"
+      type="search"
+      placeholder="Search"
+      aria-label="Search"
+    />
+    <button className="btn btn-outline-success" type="submit">
+      Search
+    </button>
+  </form>
+  )
+}
+
+function DropDown() {
+  return(
+    <ul className="navbar-nav mb-2 mb-lg-0">
           {" "}
           {/* remove ms-auto */}
           <li className="nav-item dropdown w-50">
@@ -62,6 +54,35 @@ const Navbar = () => {
             </ul>
           </li>
         </ul>
+  )
+}
+const Navbar = () => {
+  return (
+    <div>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        ⚡ Firestock
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+     <Navigation/>
+
+     <SearchForm/>
+
+     <DropDown/>
+      
+        
       </div>
     </div>
     </nav>
