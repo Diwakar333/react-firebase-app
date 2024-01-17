5f25fe5eefbaf5476e4334d01052584ecb775f9a
Diwakar <diwakar@gmail.com>
2024-01-17 13:05:15 +0530

    create and dispatch actions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,57 +15,68 @@ const initialState={
   collapse: false
 }
 
+const handleOnChange = (state, e) => {
+  if(e.target.name === 'file') {
+    return{...state.inputs, files:e.target.files[0], path:URL.createObjectURL( e.target.files[0])}
+    
+  }else{
+    return{...state.inputs, title:e.target.value}
+  }
+}
+
 function reducer(state, action) {
   switch(action.type) {
     case 'setItem':
       return{
         ...state,
-        items:[action.payload.path, ...state.items]
+        items:[state.inputs, ...state.items]
       }
+      case "setInputs":
+        return{
+          ...state,
+          inputs:handleOnChange(state, action.payload.value)
+        }
+        case "isCollapsed":
+          return{
+            ...state,
+            collapse: action.payload.bool
+          }
+          default: return state
   }
 }
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
   const [count, setCount] = useState()
-  const [inputs, setInputs] = useState({title:null, file:null, path: null})
-  const [items, setItems] = useState(photos);
-  const [collapse, isCollapse] = useState(false);
-  const toggle = () => isCollapse(!collapse)
+ 
+  const toggle = (bool) => dispatch({type:"isCollapsed", payload:{bool}})
 
-  const handleChange = (e) => {
-    if(e.target.name === 'file') {
-      setInputs({...inputs, files:e.target.files[0], path:URL.createObjectURL(e.target.files[0])})
-      
-    }else{
-      setInputs({...inputs, title:e.target.value})
-    }
-  }
+  const handleOnChange = (e) => dispatch({type: 'setInputs', payload:{value: e}})
   const handleOnSubmit = (e) => {
     e.preventDefault()
     // setItems([inputs.path, ...items]);
-    dispatch({type:'setItem', payload:{path:inputs}})
-    setInputs({title:null, file:null, path: null})
-    isCollapse(false)
+    dispatch({type:'setItem', payload:{path:state.inputs}})
+    // setInputs({title:null, file:null, path: null})
+    toggle(!state.collapse)
   }
 
-  useEffect(() =>{
-    console.log(state)
-  }, [state.items])
+  // useEffect(() =>{
+  //   console.log(state)
+  // }, [state.items])
 
   useEffect(() =>{
-    setCount(`you have ${items.length} image${items.lenght > 1 ? 's' : ''}`)
-  })
+    setCount(`you have ${state.items.length} image${state.items.lenght > 1 ? 's' : ''}`)
+  }, [state.items])
   return (
     <>
       <Navbar />
       <div className="container text-center mt-5">
-        <button className="btn btn-success float-end" onClick={toggle}>{collapse ? 'Close':'+ Add'}</button>
+        <button className="btn btn-success float-end" onClick={() => toggle(!state.collapse)}>{state.collapse ? 'Close':'+ Add'}</button>
         <div className="clearfix mb-4"></div>
         <Upload 
-        inputs={inputs}
-        isVisible={collapse}
-        onChange={handleChange}
+        inputs={state.inputs}
+        isVisible={state.collapse}
+        onChange={handleOnChange}
         onSubmit={handleOnSubmit}
         />
         {count}
